dd53dfe7fa8da6438d8d0988718f1f1004f802ae
naa <1755796+manhhomienbienthuy@users.noreply.github.com>
2022-03-28 21:33:15 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ n=0
 until [ "$n" -gt $NUM_TRIALS ]
 do
     excuse=$(curl http://programmingexcuses.com/ -s | grep -oE "<a.+>.+</a>" | sed -E 's:</?a([^>]*)?>::g')
-    grep -q "$excuse" "$OUTPUT" || (echo "$excuse" >> "$OUTPUT") && break
+    grep -q "$excuse" "$OUTPUT" || ((echo "$excuse" >> "$OUTPUT") && break)
     n=$((n+1))
     sleep 30
 done
