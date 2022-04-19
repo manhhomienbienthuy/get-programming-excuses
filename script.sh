83eff4edde08fdb5edaf9ba728396541e90aabc7
naa <1755796+manhhomienbienthuy@users.noreply.github.com>
2022-04-19 12:23:55 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,9 +8,12 @@ until [ $n -ge $NUM_TRIALS ]
 do
     echo "TRY $n"
     excuse=$(curl http://programmingexcuses.com/ -s | grep -oE "<a.+>.+</a>" | sed -E 's:</?a([^>]*)?>::g')
+    echo "$excuse"
     if ! grep -q "$excuse" "$OUTPUT"; then
         echo "$excuse" >> "$OUTPUT"
         break
+    else
+        echo "Already taken"
     fi
     n=$((n+1))
     sleep $(( ( RANDOM % 15 )  + 15 ))
